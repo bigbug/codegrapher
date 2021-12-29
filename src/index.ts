eec938a2b42d761ea51f3050cf2d315a023b0b6e
bigbug <gutbrod.markus@googlemail.com>
2021-12-29 12:22:01 +0100

    Code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { CharStreams, CommonTokenStream } from 'antlr4ts';
-import { forEach } from 'lodash';
 import {CLexer} from "./grammars/c/CLexer";
 import {BlockItemListContext, CParser} from "./grammars/c/CParser";
 import { MyCVisitor } from './MyCVisitor';
@@ -17,10 +16,6 @@ export function toTree(input: string) : BlockItemListContext {
     const tree:BlockItemListContext = parser.blockItemList();
     return tree;
 }
-
-function getKeyByValue(object:any, value:any) {
-    return Object.keys(object).find(key => object[key] === value);
-  }
 
 export function visit(input: string) {
     const v = new MyCVisitor();
@@ -77,71 +72,6 @@ export function generateDigraph(v: MyCVisitor) : string {
     a+= "}";
     return a;
 }
-
-
-/*export function generateDigraphOld(v: MyCVisitor) : string {
-    const outputs : {[key: string] : string} = {};
-    let a = "digraph {\nnode [shape=box style=filled]\ngraph [rankdir=\"LR\"]\n";
-    a += v.blocks.map((i, idx)=>{
-        const nodeid = i.type + "_" + idx;
-
-        i.outputs.forEach(j=>{
-            outputs[j] = nodeid;
-        });
-
-        if(["var", "const"].includes(i.type)) {
-            return nodeid + " [label=\""+i.configuration+"\"]"
-        } else if(["function"].includes(i.type)) {
-            return nodeid + " [label=\""+i.configuration+"\" color=green]"
-        } else if (["sum", "multiply", "shift"].includes(i.type)) {
-            return nodeid + " [label=\""+(i.configuration as string[]).join("\\n")+"\"]"
-        } else if (["relational"].includes(i.type)) {
-            return nodeid + " [label=\""+(i.configuration as string)+"\"]"
-        } else if (["and", "or"].includes(i.type)) {
-            return nodeid + " [label=\""+(i.type==="and" ? "&&" : "||")+"\"]"
-        } else if (["abs", "not"].includes(i.type)) {
-            return nodeid + " [label=\""+i.type+"\"]"
-        } else if (["gain"].includes(i.type)) {
-            return nodeid + " [label=\""+i.configuration+"\"]"
-        } else if (["switch"].includes(i.type)) {
-            return nodeid + " [label=\"1\\ncond\\n2\"]"
-        } else if (["multiswitch"].includes(i.type)) {
-            return nodeid + " [label=\"cond\\n"+i.configuration+"\"]"
-        } else {
-            throw new Error("Unsupported node of type '"+i.type+"'");
-        }
-        return nodeid;
-    }).join("\n") + "\n";
-
-    let c = 0;
-    forEach(v.vars, (key:string, v)=>{
-        if(!outputs[key]) {
-            outputs[key] = "var_unused_" + c++;
-            a += outputs[key] + " [label=\""+v+"\"]\n";
-        }
-    })
-
-    v.blocks.map((i, idx) => {
-        const nodeid = i.type + "_" + idx;
-
-        i.inputs.forEach(j=>{
-            const label = v.varHistory[j] ? " [label=\""+v.varHistory[j]+"\"]" : "";
-            const z = outputs[j];
-            a+= z + " -> " + nodeid + label + "\n";
-        });
-
-        /*i.outputs.forEach(j => {
-            if(Object.values(v.vars).includes(j)) {
-                const x = "var_unused_" + c++;
-                a += x + " [label=\""+getKeyByValue(v.vars, j)+"\"]\n";
-                a += nodeid + " -> " + x + "\n";
-            }
-        })*//*
-    });
-
-    a+= "}";
-    return a;
-}*/
 
 export default function getDigraph(input: string) : string {
     const v = visit(input);
